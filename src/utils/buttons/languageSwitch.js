617866bb91d45338bfa1e59e4158b29066011ced
David Dupraz <ddupraz@lab4tech.lan>
2025-04-09 13:59:32 +0200

    Button UX working

--- src/utils/buttons/languageSwitch.js
+++ src/utils/buttons/languageSwitch.js
@@ -4,23 +4,25 @@ import EngIcon from '../../resources/icons/eng-icon.png';
 import FrIcon from '../../resources/icons/fr-icon.png';
 
 const LanguageSwitch = () => {
-    const [isClicked, setIsClicked] = useState(false);
+    const [isClicked, setIsClicked] = useState(true);
 
   const handleLanguageChange = (e) => {
     setIsClicked(!isClicked)
   };
 
-  return (<div id="flagList">
-    <button onClick={handleLanguageChange}><img src={EngIcon}
-    className="langFlag"
-    alt="UK flag icon" />
-    </button>
-    {isClicked &&<>
-    <button><img src={FrIcon} alt="France flag icon" className="langFlag"/></button>
-    <button><img src={EngIcon} className="langFlag"/></button>
+  return (<div id="flagList"
+    style={{transform : isClicked ? "translateY(26%)" : "translateY(0%)"}}>
+        <button onClick={handleLanguageChange}><img src={EngIcon}
+        className="langFlag"
+        alt="UK flag icon" />
+        </button>
+        {isClicked &&<>
+        <button><img src={FrIcon} alt="France flag icon" className="langFlag"/></button>
+        
     </>}
     </div>
   );
+  //<button><img src={EngIcon} className="langFlag"/></button>
 };
 
 export default LanguageSwitch;
