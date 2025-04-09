5a09e10a36710db79ade02d6dc719bad150e0575
David Dupraz <ddupraz@lab4tech.lan>
2025-04-09 14:19:21 +0200

    transmit parameter

--- src/utils/buttons/languageSwitch.js
+++ src/utils/buttons/languageSwitch.js
@@ -5,24 +5,24 @@ import FrIcon from '../../resources/icons/fr-icon.png';
 
 const LanguageSwitch = () => {
     const [isClicked, setIsClicked] = useState(true);
+    const en = 'en', fr = 'fr';
 
   const handleLanguageChange = (e) => {
+    console.log(e);
     setIsClicked(!isClicked)
   };
 
   return (<div id="flagList"
     style={{transform : isClicked ? "translateY(26%)" : "translateY(0%)"}}>
-        <button onClick={handleLanguageChange}><img src={EngIcon}
-        className="langFlag"
-        alt="UK flag icon" />
+        <button onClick={() => handleLanguageChange(en)}>
+            <img src={EngIcon} className="langFlag" alt="UK flag icon" />
         </button>
         {isClicked &&<>
-        <button><img src={FrIcon} alt="France flag icon" className="langFlag"/></button>
-        
+        <button onClick={() => handleLanguageChange(fr)}>
+            <img src={FrIcon} alt="France flag icon" className="langFlag"/></button>
     </>}
     </div>
   );
-  //<button><img src={EngIcon} className="langFlag"/></button>
 };
 
 export default LanguageSwitch;
