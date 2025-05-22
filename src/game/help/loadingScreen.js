8644df0cbadf5ae0dcb59fd492322762da9056da
David Dupraz <duprazdavid@gmail.com>
2025-05-22 11:31:55 +0200

    Carousel works

--- src/game/help/loadingScreen.js
+++ src/game/help/loadingScreen.js
@@ -1,20 +1,25 @@
 import React from 'react'
-import { CCarousel, CCarouselItem, CImage } from '@coreui/react'
 import useEmblaCarousel from 'embla-carousel-react';
+import Autoplay from 'embla-carousel-autoplay';
+import Fade from 'embla-carousel-fade';
 
 const LoadingScreen = () => {
+    const flagFiles = [
+        'empire.png',
+        'rebels.png',
+        'chiss_ascendancy.svg'];
 
     function MainCarousel() {
-        const [emblaRef] = useEmblaCarousel();
+        const [emblaRef] = useEmblaCarousel({ loop: true },
+            [Autoplay({ playOnInit: true, delay: 3000 }), Fade({ duration: 1500})]);
 
         return <div className='embla overflow-hidden' ref={emblaRef}>
             <div className='embla__container flex'>
-                <div className='embla__slide flex-[0_0_100%] min-w-0 flex justify-center'>
-                    <img className='h-48' src='/resources/flags/empire.png' />
-                </div>
-                <div className='embla__slide flex-[0_0_100%] min-w-0 flex justify-center'>
-                    <img className='h-48' src='/resources/flags/rebels.png' />
-                </div>
+                {flagFiles.map((file) => (
+                    <div className='embla__slide flex-[0_0_100%] min-w-0 flex justify-center' key={file}>
+                        <img className='embla__slide__img d-block h-48' src={`/resources/flags/${file}`} />
+                    </div>
+                ))}
             </div>
         </div>
     }
