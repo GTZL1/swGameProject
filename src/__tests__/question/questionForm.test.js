4c639da4008aaf96adc1b3f276a6805f1279b079
David Dupraz <duprazdavid@gmail.com>
2025-05-23 15:45:38 +0200

    Add first 3 tests

--- src/__tests__/question/questionForm.test.js
+++ src/__tests__/question/questionForm.test.js
@@ -1,7 +1,8 @@
 import '@testing-library/jest-dom';
-import { render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import axios from 'axios';
 import QuestionForm from '../../game/question/questionForm.js';
+import React, { useState, useEffect } from 'react';
 
 jest.mock('axios');
 jest.mock('../../game/help/loadingScreen.js', () => () => <div>Loading...</div>);
@@ -15,6 +16,14 @@ jest.mock('../../context/FontContext.js', () => ({
     }),
 }));
 
+beforeEach(async () => {
+  axios.get.mockResolvedValue(question);
+  render(<QuestionFormWrapper />);
+  await screen.findByText(new RegExp(question.data.questionTitle, 'i'));
+});
+
+const wrongAnswer = "Too bad";
+const rightAnswer = "Correct";
 const question = {
     data: {
         documentId: '1',
@@ -25,9 +34,47 @@ const question = {
     }
 }
 
+function answerMessage(isCorrect) {
+    return (isCorrect ? rightAnswer : wrongAnswer);
+}
+
+function QuestionFormWrapper() {
+    const [isCorrect, setIsCorrect] = React.useState(null);
+    return (
+        <QuestionForm
+            questionDocId="1"
+            isCorrect={isCorrect}
+            setIsCorrect={setIsCorrect}
+            answerProps={isCorrect !== null && (<p>{answerMessage(isCorrect)}</p>)}
+        />
+    );
+}
+
 test('loads and displays question', async () => {
+    expect(screen.queryByText(new RegExp(question.data.questionTitle, 'i'))).toBeInTheDocument();
+});
+
+test('submit no answer', async () => {
+    fireEvent.click(screen.getByRole('button', { name: /questions/i }));
+
+    expect((screen.getByRole('textbox').value)).toBe(question.data.answer, 'i');
+    expect(screen.queryByText(new RegExp(wrongAnswer, 'i'))).toBeInTheDocument();
+});
+
+test('submit right answer', async () => {
+    fireEvent.change(screen.getByRole('textbox'), { target: { value: question.answer } });
+    fireEvent.click(screen.getByRole('button', { name: /questions/i }));
+
+    expect((screen.getByRole('textbox').value)).toBe(question.data.answer, 'i');
+    expect(screen.queryByText(new RegExp(wrongAnswer, 'i'))).toBeInTheDocument();
+});
+/*
+test('loading screen works properly', async () => {
+    render(<QuestionFormWrapper />);
+    expect(await screen.findByText(new RegExp('Loading', 'i'))).toBeInTheDocument();
+
     axios.get.mockResolvedValue(question);
 
-    render(<QuestionForm questionDocId="1" isCorrect={null} setIsCorrect={() => {}} />);
+    expect(await screen.findByText(new RegExp('Loading', 'i'))).not.toBeInTheDocument();
     expect(await screen.findByText(new RegExp(question.data.questionTitle, 'i'))).toBeInTheDocument();
-});
+});*/
