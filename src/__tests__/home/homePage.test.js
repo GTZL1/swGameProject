57c9bfa4466218f3fa0ff88699bb60d6c1454955
David Dupraz <duprazdavid@gmail.com>
2025-05-27 13:54:18 +0200

    Add tests

--- src/__tests__/home/homePage.test.js
+++ src/__tests__/home/homePage.test.js
@@ -1,9 +1,12 @@
 import '@testing-library/jest-dom';
 import { fireEvent, render, screen } from '@testing-library/react';
+import axios from 'axios';
 import HomePage from '../../home/homePage';
-import { BrowserRouter, MemoryRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router";
+import QuestionPage from '../../game/question/questionPage';
 
 jest.mock('axios');
+jest.mock('../../game/help/loadingScreen.js', () => () => <div>Loading...</div>);
 jest.mock('../../context/FontContext.js', () => ({
     useFont: () => ({
         contentFont: 'mock-font',
@@ -14,23 +17,46 @@ jest.mock('../../context/FontContext.js', () => ({
     }),
 }));
 
+const question = {
+    data: {
+        documentId: '1',
+        questionTitle: `What's the name of Sabine's loth-cat ?`,
+        answer: 'Murley',
+        indication: 'Name',
+        imageUrl: '/murley.png'
+    }
+}
+
+function onHomePage() {
+    expect(screen.getByRole('button', { name: /questions/i})).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /identity/i})).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /challenges/i})).toBeInTheDocument();
+}
+
 test('load home page', async () => {
     render(<BrowserRouter>
         <HomePage />
     </BrowserRouter>);
 
-    expect(screen.getByRole('button', { name: /questions/i})).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: /identity/i})).toBeInTheDocument();
-    expect(screen.getByRole('button', { name: /challenges/i})).toBeInTheDocument();
+    onHomePage();
 });
 
 test('navigates to question page on button click', async () => {
-    render(<MemoryRouter initialEntries={['/']}>
-        <HomePage />
-    </MemoryRouter>);
+    axios.get.mockResolvedValueOnce({ data : ['1','2'] });
+    axios.get.mockResolvedValueOnce(question);
+    render(<BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/question" element={<QuestionPage />} />
+        </Routes>
+    </BrowserRouter>);
+    expect(screen.getByRole('button', { name: /questions/i})).toBeInTheDocument();
 
-    const questionButton = screen.getByRole('button', { name: /question/i });
-    await userEvent.click(questionButton);
+    fireEvent.click(screen.getByRole('button', { name: /questions/i }));
 
-    expect(window.location.pathname).toBe('/question');
+    await screen.findByText(new RegExp(question.data.questionTitle, 'i'));
+
+    fireEvent.click(screen.getByRole('link'));
+
+   onHomePage();
 });
