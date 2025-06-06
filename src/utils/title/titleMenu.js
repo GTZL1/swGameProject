be4f7de5821c4b28d0a2de3a803615666eae6b4b
David Dupraz <duprazdavid@gmail.com>
2025-06-06 11:47:04 +0200

    Fix font switch

--- src/utils/title/titleMenu.js
+++ src/utils/title/titleMenu.js
@@ -1,11 +1,13 @@
 import { Link } from "react-router";
 import { useTranslation } from "react-i18next";
+import { useFont } from "../../context/FontContext.js";
 import ENDPOINTS from "../../constants/endpoints.js";
 
 const TitleMenu = ({style}) => {
     const { t } = useTranslation();
+    const { contentFont } = useFont();
 
-    return (<nav className={`${style} flex flex-col sm:flex-row sm:justify-around items-center`}>
+    return (<nav className={`${style} ${contentFont} flex flex-col sm:flex-row sm:justify-around items-center shadow-md`}>
         <MenuText text={t('titles.about_title')} />
         <MenuText text={t('titles.how_to_title')} />
         <MenuText text={t('titles.support_title')} />
@@ -15,7 +17,7 @@ const TitleMenu = ({style}) => {
 
 const MenuText = ({text, route}) => {
     return (<Link to={route}>
-        <p className="font-bold text-sm py-1">
+        <p className={`font-bold text-sm py-1`}>
             {text}
         </p>
     </Link>);
