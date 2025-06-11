1f60fa8ed61f5531a10b5ae63547cb0931332a13
David Dupraz <duprazdavid@gmail.com>
2025-06-11 10:19:34 +0200

    Create a first Lightbox

--- src/info/howToPage.js
+++ src/info/howToPage.js
@@ -6,11 +6,17 @@ import axios from "axios";
 import ENDPOINTS from "../constants/endpoints.js";
 import { HOW_TO_IMAGE_QUESTIONS, SEPARATOR_IN_FILES } from "../constants/constants.js";
 import ICONS from "../constants/icons.js";
+import Lightbox from "yet-another-react-lightbox";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
 
 const HowToPage = () => {
     const {i18n, t} = useTranslation();
     const {contentFont} = useFont();
     const [questionHelp, setQuestionHelp] = useState(null);
+    const [isOpen, setIsOpen] = useState(false);
+    const [photoIndex, setPhotoIndex] = useState(0);
 
     useEffect(() => {
         axios
@@ -25,8 +31,13 @@ const HowToPage = () => {
                 <p>{t('how_to.intro')}</p>
                 <Subtitle text={t('titles.question_title')} />
                 {questionHelp && <HelpParagraph text={questionHelp} />}
-                <Images images={HOW_TO_IMAGE_QUESTIONS} />
+                <Images images={HOW_TO_IMAGE_QUESTIONS} setIsOpen={setIsOpen} setPhotoIndex={setPhotoIndex} />
             </article>
+            <Lightbox open={isOpen}
+                close={() => setIsOpen(false)}
+                slides={HOW_TO_IMAGE_QUESTIONS.map((i) => {return { src: `${ICONS.PATH}${i}`};})}
+                index={photoIndex}
+            />
         </section>
     </>);
 }
@@ -35,10 +46,15 @@ const Subtitle = ({text}) => {
     return <p className='text-cyan-600 font-bold text-lg text-center mt-5'>{text}</p>
 }
 
-const Images = ({images}) => {
+const Images = ({images, setIsOpen, setPhotoIndex}) => {
     return (<div className="flex flex-col md:flex-row gap-2 md:justify-center items-center mt-2">
         {images.map((img, idx) => <img className="min-w-36 md:max-w-[40%] max-w-[80%]"
-            key={idx} src={`${ICONS.PATH}${img}`} />)}
+            key={idx} src={`${ICONS.PATH}${img}`}
+            onClick={() => {
+                setIsOpen(true);
+                setPhotoIndex(idx);
+            }
+        }    />)}
     </div>);
 }
 
