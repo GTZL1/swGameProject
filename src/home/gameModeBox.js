411020f202152705ddacf3fe2495a5418b2cf58f
David Dupraz <duprazdavid@gmail.com>
2025-06-12 13:04:16 +0200

    fix home page for real fans

--- src/home/gameModeBox.js
+++ src/home/gameModeBox.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from 'react';
 import ButtonLink from '../utils/buttons/buttonLink.js';
 
 const GameModeBox = ({endpoints, title, text, buttonTexts}) => {
-    return (
-        <div className={`w-80 h-60 bg-slate-400 bg-opacity-60 flex flex-col items-center justify-center gap-1 p-3 border-4 border-solid border-black m-5`}>
-            <div className={`h-[50%] flex flex-col items-center text-center`}>
+    return (<div
+            className={`w-80 min-h-60 bg-slate-400 bg-opacity-60 flex flex-col justify-between gap-1 p-3 border-4 border-solid border-black m-5`}>
+            <div className={`flex flex-col items-center text-center`}>
                 <BoxText title={title} text={text} />
             </div>
             <Buttons endpoints={endpoints} buttonTexts={buttonTexts} />
@@ -20,7 +20,7 @@ const BoxText = ({title, text}) => {
 }
 
 const Buttons = ({endpoints, buttonTexts}) => {
-    return (<div className='h-[50%] flex flex-col items-center justify-around'>
+    return (<div className='min-h-24 flex flex-col items-center justify-center gap-3'>
         {endpoints.map((endpoint, index) =>
             <ButtonLink route = {endpoint} key={index}
                 style="text-red-600 text-lg border-2 border-solid border-red-600 px-2">
